b2d8997c8cf35c745eb8702a157251320f474814
Armand Philippot <git@armandphilippot.com>
2022-01-21 18:32:04 +0100

    build: add scripts to bump all necessary files on new release
    WordPress requires that readme.txt and comment inside main plugin file
    are bumped on new release. I also declare a PHP constant that needs to
    be increment.

---
+++ tools/bump-php-comment.js
@@ -0,0 +1,19 @@
+/**
+ * bump-php-comment.js
+ *
+ * Copy package.json version into apcom-cpt.php file.
+ */
+const fs = require( 'fs' );
+
+// Load package.json data.
+const packageJson = JSON.parse( fs.readFileSync( './package.json' ) );
+const packageVersion = packageJson.version;
+
+module.exports = {
+	files: 'apcom-cpt.php',
+	from: new RegExp(
+		'Version: {11}(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?',
+		'g'
+	),
+	to: 'Version:           ' + packageVersion,
+};
